a09b96586acb665335eaceee8d400aafd0b54128
Eli Sacks <bastlifa@gmail.com>
2019-07-26 12:23:04 -0400

    Part 1 Completed, er, now with location styling

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,29 @@
 import React, { useEffect, useState } from 'react';
+import LocationCard from './LocationCard'
+import axios from "axios"
+import { LocationGrid } from './StyledComps'
 
 export default function LocationsList() {
+    
+    const [locations, setLocations] = useState([])
 
+    useEffect(() => {
+        // TODO: Add AJAX/API Request here - must run in `useEffect`
+        //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+        axios.get(`https://rickandmortyapi.com/api/location/`)
+        .then(response => {
+            setLocations(response.data.results)
+            console.log(response)
+        })
+    }, [])
+
+        return <LocationGrid>
+        {locations === [] ? <h1>Loading locations...</h1> :
+            locations.map((location, index) => <LocationCard location={location} key={index} />)
+        }
+        </LocationGrid>
 }
+
+
+
+
--- src/components/StyledComps.js
+++ src/components/StyledComps.js
@@ -26,3 +26,40 @@ export const EpisodeLinkDiv = styled.div`
 export const GreyLink = styled(Link)`
     color: gray;
 `;
+
+export const LocationGrid = styled.div`
+    margin-top: 20px;
+    display: grid;
+    justify-items: center;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 30px;
+`;
+
+export const LocationCardDiv = styled.div`
+    width: 300px;
+    border: 2px solid lightgrey;
+    border-radius: 8px;
+    padding: 10px;
+`;
+
+export const GreyHR = styled.hr`
+    border-color: #E1E1E1;
+    border-top: none;
+    width: 100%;
+    margin-left: -10px;
+`;
+
+export const HRResDiv = styled.div`
+    display: flex;
+
+`;
+
+export const ResidentDiv = styled.div`
+    width: 150px;
+    padding-top: 6px;
+    padding-left: 6px;
+    margin-right: -10px;
+    margin-bottom: -10px;
+    background-color: lightgrey;
+    border-top-left-radius: 8px;
+`;
